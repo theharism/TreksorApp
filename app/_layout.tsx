2598e92412a0691365f3730d55583e20a5b09d8b
theharism <chharis9999@gmail.com>
2025-05-18 15:51:03 +0500

    move auth files to (auth)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,26 @@
-// _layout.jsx
-import { ImageBackground } from "expo-image";
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { Dimensions, StyleSheet, View } from "react-native";
+// app/_layout.tsx
+import { useAuthStore } from "@/store/auth-store";
+import { Slot } from "expo-router";
+import { useEffect } from "react";
 import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
+
+  const { error, clearError } = useAuthStore()
+
+  // Show toast when auth error changes
+  useEffect(() => {
+    if (error) {
+      Toast.show({
+        type: "error",
+        text1: "Error",
+        text2: error,
+        onHide: clearError,
+      })
+    }
+  }, [error, clearError])
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <ImageBackground
-        source={require("../assets/images/onboarding.png")}
-        contentFit="cover"
-        style={styles.backgroundImage}
-        imageStyle={styles.imageStyle}
-      >
-          <Stack
-            screenOptions={{
-              headerShown: false,
-              headerTransparent: true,
-              headerStyle: {
-                backgroundColor: 'transparent',
-              },
-              headerTintColor: '#fff',
-              contentStyle: {
-                backgroundColor: 'transparent',
-              },
-              animation: "none"
-            }}
-          >
-            <Stack.Screen name="index" />
-            <Stack.Screen name="login" />
-            <Stack.Screen name="register" />
-            <Stack.Screen name="request-reset-password" />
-            <Stack.Screen name="reset-password" />
-            <Stack.Screen name="verify-otp" />
-            {/* Other screens */}
-          </Stack>
-          <Toast />
-      </ImageBackground>
-    </View>
+        <Slot />
   );
 }
-
-const { width, height } = Dimensions.get("window");
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  backgroundImage: {
-    width,
-    height,
-  },
-  imageStyle: {
-    opacity: 0.9,
-  },
-});
