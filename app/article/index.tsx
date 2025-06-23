87922840009b7ab38779d70c204c788b5422ce19
theharism <chharis9999@gmail.com>
2025-06-24 02:17:25 +0500

    articles nav fixed

--- app/article/index.tsx
+++ app/article/index.tsx
@@ -102,9 +102,7 @@ export default function ArticlesScreen({ mode = "tab", category = "All Articles"
   };
 
   const handleArticlePress = (article: Article) => {
-    // Navigate to article detail screen
-    console.log("Navigate to article:", article.title);
-    const path = activeCategory === 'All Articles' ? `/(tabs)/articles/${article._id}` : `/${category.toLowerCase()}/articles/${article._id}`;
+    const path = activeCategory === 'All Articles' ? `/(tabs)/articles/${article._id}` : `/${activeCategory.toLowerCase()}/articles/${article._id}`; 
     router.push(path);
   };
 
