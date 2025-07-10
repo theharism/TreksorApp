8109bb071ffd3f61b15fad4fe44e7d6f43a7ffa3
theharism <chharis9999@gmail.com>
2025-07-11 02:13:13 +0500

    updated workout background

--- app/body/workout/index.tsx
+++ app/body/workout/index.tsx
@@ -12,7 +12,7 @@ const Selection = () => {
     <View style={styles.container}>
       <StatusBar style="light" />
       <ImageBackground
-        source={require("@/assets/images/body-home.png")}
+        source={require("@/assets/images/workout-home.jpeg")}
         contentFit="cover"
         style={styles.backgroundImage}
         imageStyle={styles.imageStyle}
